c47def0fd402418f9f22ba8d86ae4759acc19a6d
myqz <ambition292@163.com>
2021-12-30 00:05:53 +0800

    reset master

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,22 @@
 import React from 'react';
 
 import Product from './components/product/product';
-
 import ShowDialog from './components/showDialog';
 import ShowInput from './components/showInput';
 import Test from './Test';
 import OuterClickExample from './components/OuterClickExample';
 import ContextDemo from './examples/context-demo';
 
-import { Inspector } from 'react-dev-inspector';
-
-const InspectorWrapper = process.env.NODE_ENV === 'development' ? Inspector : React.Fragment;
-
 function App() {
   return (
-    <InspectorWrapper
-      keys={['control', 'shift', 'q']} // default keys
-    >
-      <div>
-        <ShowInput />
-        <ShowDialog />
-        <Product />
-        <Test />
-        <OuterClickExample />
-        <ContextDemo />
-      </div>
-    </InspectorWrapper>
+    <div>
+      <ShowInput />
+      <ShowDialog />
+      <Product />
+      <Test />
+      <OuterClickExample />
+      <ContextDemo />
+    </div>
   );
 }
 
